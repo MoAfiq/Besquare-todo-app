d5f92469cee6c06b24fe9b7399eecab25047f7bc
Afiq Salleh <afiqsalleh97@gmail.com>
2022-07-12 09:18:12 +0800

    Trying to integrate gender select element

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [name, setName] = React.useState<string>("");
+  const [gender, setGender] = React.useState<string | undefined>("Male");
+  const [age, setAge] = React.useState<string | undefined>("");
+  const [color, setColor] = React.useState<string | undefined>("red");
+
+  const changeName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+
+  const changeGender = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setGender(event.target.value);
+  };
+
+  const changeAge = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAge(event.target.value);
+  };
+
+  const changeColor = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setColor(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(name, gender, age, color);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="pa-16">
+      <h2>Fill the Form!</h2>
+      <form onSubmit={handleSubmit}>
+        <Input name="Name" value={name} onChange={changeName} />
+        <Select name="Gender" value={gender} onChange={changeGender} />
+        <Input name="Age" value={age} onChange={changeAge} />
+        <Select name="Favourite Color" value={color} onChange={changeColor} />
+        <button type="submit" className="btn-primary mb-16">
+          Submit
+        </button>
+        <button className="btn-secondary">Clear</button>
+      </form>
     </div>
   );
 }
 
-export default App;
+type InputType = {
+  name: string;
+  value: string | undefined;
+  onChange: (change: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+const Input = ({ value, onChange, name }: InputType) => {
+  return (
+    <div className="mb-16">
+      <label>{name}</label>
+      <input value={value} onChange={onChange} />
+    </div>
+  );
+};
+
+type SelectType = {
+  name: string;
+  value: string | undefined;
+  onChange: (changeColor: React.ChangeEvent<HTMLSelectElement>) => void;
+};
+
+const Select = ({ value, onChange, name }: SelectType) => {
+  return (
+    <div className="mb-16">
+      <label>{name}</label>
+      <select value={value} onChange={onChange}>
+        <option value="red">Red</option>
+        <option value="blue">Blue</option>
+        <option value="skyblue">Sky Blue</option>
+      </select>
+    </div>
+  );
+};
+
+//  <label>{name}</label>
+//       <select value={value} onChange={onChange}>
+//         <option value="male">Male</option>
+//         <option value="female">Female</option>
+//       </select>
